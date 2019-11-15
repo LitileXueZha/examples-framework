e783ebd2cf6bf522a9f54fcf9d4a1a2c8fc4ac4d
宁涛 <tao.n@shulidata.com>
2019-11-15 18:26:16 +0800

    3. 添加 angular

---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appID: '1:13839092631:web:9f8fa7631c7e02bcd152d2',
+  gKey: '',
+  config: {
+    apiKey: 'AIzaSyB6vqqhtrIfv7009Gu5gonIP7bpMzxNtFE',
+    authDomain: 'helloworld-c8a99.firebaseapp.com',
+    databaseURL: 'https://helloworld-c8a99.firebaseio.com',
+    projectId: 'helloworld-c8a99',
+    storageBucket: 'helloworld-c8a99.appspot.com',
+    messagingSenderId: '13839092631'
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
